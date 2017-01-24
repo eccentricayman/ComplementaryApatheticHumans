a177f4ce068a5c1ea859c66785e1b5c7eb6386e1
eccentricayman <aahmed3@stuy.edu>
2017-01-23 23:16:46 -0500

    cards

--- cards.c
+++ cards.c
@@ -56,17 +56,32 @@ char * getBlackCard() {
     return NULL;
 }
 
-int countOccurences(char * string) {
+int countOccurences(char * string, char * toFind) {
     int ctr = 0;
     char * temp = string;
-    while((temp = strstr(temp, "<i>"))) {
+    while((temp = strstr(temp, toFind))) {
         ctr++;
         temp++;
     }
     return ctr;
 }
 
-char * replace(char * str, char * toReplace, char * replacingWith) {
+char * replace(char *str, char *toReplace, char * replacingWith) {
+  static char buffer[4096];
+  char *p;
+
+  if(!(p = strstr(str, toReplace))) {
+      return str;
+  }
+  strncpy(buffer, str, p-str);
+  buffer[p-str] = '\0';
+
+  sprintf(buffer+(p-str), "%s%s", replacingWith, p+strlen(toReplace));
+
+  return buffer;
+}
+
+char * replaceAll(char * str, char * toReplace, char * replacingWith) {
     char *result; //retstr
     char *ins; //next insert point
     char *tmp;    
@@ -114,12 +129,58 @@ void printCard(char * card) {
     char *localCard = malloc (strlen (card) * 2);
     localCard = card;
     //replace for <i>
-    localCard = replace(localCard, "<i>", "\e[3m");
+    localCard = replaceAll(localCard, "<i>", "\e[3m");
     //replace for </i>
-    localCard = replace(localCard, "</i>", "\e[0m");
+    localCard = replaceAll(localCard, "</i>", "\e[0m");
     //replace <br>
-    localCard = replace(localCard, "<br>", "\n");
+    localCard = replaceAll(localCard, "<br>", "\n");
     //print
     printf("%s", localCard);
     free(localCard);
+}
+
+char * combineCards(char * black, char * white1, char * white2) {
+    char * retP = malloc(strlen(black) + strlen(white1) + strlen(white2) + 1);
+    retP = black;
+
+    //coloring card1
+    char * w1Local = malloc(strlen(white1) + 10);
+    strcat(w1Local, "\e[34m");
+    strcat(w1Local, white1);
+    strcat(w1Local, "\e[0m");
+    
+    //coloring card2
+    char * w2Local = malloc(strlen(white2) + 10);
+    strcat(w2Local, "\e[34m");
+    strcat(w2Local, white2);
+    strcat(w2Local, "\e[0m");
+    
+    //check if 1 card to replace or two
+    int count = countOccurences(black, "_");
+    if (count == 1) {
+        w1Local = replaceAll(w1Local, ".", "\0");
+        char * index = strrchr(w1Local, '\n');
+        w1Local[index - w1Local] = '\e';
+        
+        retP = replaceAll(retP, "_", w1Local);
+    }
+    else if (count == 2) {
+        w1Local = replaceAll(w1Local, ".", "\0");
+        char * index = strrchr(w1Local, '\n');
+        w1Local[index - w1Local] = '\e';
+        w2Local = replaceAll(w2Local, ".", "\0");
+        char * index2 = strrchr(w2Local, '\n');
+        w2Local[index2 - w2Local] = '\e';
+        
+        retP = replaceAll(retP, "_", w2Local);
+        retP = replace(retP, w2Local, w1Local);
+    }
+    else {
+        //when there's no prompt
+        strcat(retP, w1Local);
+    }
+    //freeing colored local copies
+    free(w1Local);
+    free(w2Local);
+    return retP;
 }
--- cards.h
+++ cards.h
@@ -19,8 +19,17 @@ char * getWhiteCard();
 //Returns random black card as a string and removes it from cards
 char * getBlackCard();
 
-//replaces part of a string with another
-char * replace(char * str, char * toReplace, char * replacingWith);
+//count how many times tofind occurs in string
+int countOccurences(char * string, char * toFind);
+
+//replace first instance of a string with another
+char * replace(char * str , char * toReplace, char * replacingWith);
+
+//replaces all instances of a string with another
+char * replaceAll(char * str, char * toReplace, char * replacingWith);
 
 //prints a card, with newlines, italics, and other formatting
 void printCard(char * card);
+
+//combines a black card and white card
+char * combineCards(char * black, char * white1, char * white2);
